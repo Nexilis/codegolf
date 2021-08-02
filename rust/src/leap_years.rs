76d1f0f50300c750e9dfea4a308a02c9f7c200e8
Bartek Łukasik <Nexilis@users.noreply.github.com>
2021-08-02 10:13:09 +0200

    Leap years compressed for code.golf

--- rust/src/leap_years.rs
+++ rust/src/leap_years.rs
@@ -4,6 +4,9 @@
 // In the Gregorian calendar, a leap year is created by extending February to 29 days in order to keep the calendar year synchronized with the astronomical year. These longer years occur in years which are multiples of 4, with the exception of centennial years that aren’t multiples of 400.
 // Write a program to print all the leap years from the year 1800 up to and including 2400.
 
+// resulting code compressed for code.golf:
+// fn main(){for i in 1800..2401{let a=if i%100==0{if i%400==0{1}else{0}}else if i%4==0{1}else{0};if a==1{println!("{}",i);}}}
+
 pub fn main() {
     for i in 1800..2401 {
         let a = is_leap(i);
